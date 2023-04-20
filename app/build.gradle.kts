d4598d8945a8b42ebf1b6fe82beff3eb14e3b6a2
priyanshuborole <ppborole1762002@gmail.com>
2023-04-20 15:00:34 +0530

    :zap: Setup central project dependency module using kotlin Dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins{
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android{
+
+    namespace = "com.priyanshub.messagemate"
+    compileSdk = Versions.compileSdk
+
+    defaultConfig{
+        applicationId = "com.priyanshub.messagemate"
+        minSdk = Versions.minSdk
+        targetSdk = Versions.targetSdk
+        versionCode = Versions.versionCode
+        versionName = Versions.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes{
+        getByName("release"){
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions{
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    dependencies{
+        implementation(Dependencies.coreKtx)
+        implementation(Dependencies.appcompat)
+        implementation(Dependencies.material)
+        implementation(Dependencies.constraintLayout)
+        testImplementation(Dependencies.junit)
+        androidTestImplementation(Dependencies.extJunit)
+        androidTestImplementation(Dependencies.espressoCore)
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,15 @@
+object Versions {
+    val coreKtx = "1.7.0"
+    val appcompat = "1.6.1"
+    val material = "1.8.0"
+    val constraintLayout = "2.1.4"
+    val junit = "4.13.2"
+    val extJunit = "1.1.5"
+    val espressoCore = "3.5.1"
+
+    val compileSdk = 33
+    val minSdk = 24
+    val targetSdk = 33
+    val versionCode = 1
+    val versionName = "1.0"
+}
